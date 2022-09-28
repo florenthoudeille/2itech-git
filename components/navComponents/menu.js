a8e1d7e1cf5d949701bf0a4c609efe582aa399e6
Your Name <you@example.com>
2022-09-28 15:26:53 +0200

    change structure

---
+++ components/navComponents/menu.js
@@ -0,0 +1,20 @@
+const menu = {
+    liHome: {
+      name: 'li',
+      text: 'Home',
+      attribut: {
+        class: 'cursor-pointer',
+        id: 'btnHome',
+      },
+    },
+    liProducts: {
+      name: 'li',
+      text: 'Products',
+      attribut: {
+        class: 'cursor-pointer',
+        id: 'btnProducts',
+      },
+    }
+}
+
+export const MenuComponent = menu;
